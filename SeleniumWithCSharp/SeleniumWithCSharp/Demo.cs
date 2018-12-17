370bb51d5b402b0942df5f77c0307deca5bd4592
jainmayankengr <jainmayankengr@gmail.com>
2018-12-17 23:42:18 +0530

    First Commit

---
+++ SeleniumWithCSharp/SeleniumWithCSharp/Demo.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWithCSharp
+{
+    class Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\SeleniumWithCSharp\\ThirdParty\\chrome");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SeleniumWithCSharp/Steps/FirstFeatureFileSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumWithCSharp.Steps
+{
+    [Binding]
+    public class FirstFeatureFileSteps
+    {
+        [Given(@"Open Browser")]
+        public void GivenOpenBrowser()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
